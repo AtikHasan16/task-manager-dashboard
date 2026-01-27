4a22b175374d6f17af25f066767c0aa73343138c
Hasan MD Atik <akhasannaeem@gmail.com>
2026-01-28 00:11:48 +0600

    feat: added navbar with avatar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
+import { Bell } from "lucide-react";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="text-white bg-primary py-7 px-5">
-      <h1>Dashboard Overview</h1>
+    <div className="text-white bg-primary py-5 px-5 flex justify-between items-center">
+      <div>
+        <h1 className="text-[32px]">Dashboard Overview</h1>
+      </div>
+      <div className="flex items-center gap-10">
+        <Bell></Bell>
+        <figure className="w-20 h-20 rounded-full overflow-hidden">
+          <img src="/AvatarStyle6.png" alt="avatar Image" />
+        </figure>
+      </div>
     </div>
   );
 };
